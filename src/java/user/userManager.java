6afe98b5a72b913b5a5022056660a2ae330bfc6b
Rui <Rui@etoile-pc>
2012-03-14 11:53:48 +0000

    profile e menu  a comecar a ficar funcional

---
+++ src/java/user/userManager.java
@@ -0,0 +1,153 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package user;
+
+import etoile.javapi.professor.Discipline;
+import etoile.javapi.professor.Professor;
+import etoile.javapi.professor.ServiceManager;
+import java.io.Serializable;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import menu.MenuBean;
+import org.primefaces.component.menuitem.MenuItem;
+import org.primefaces.component.submenu.Submenu;
+import sha1.sha1;
+
+@ManagedBean(name = "userManager")
+@SessionScoped
+public class userManager implements Serializable {
+
+    @ManagedProperty(value = "#{sha1}")
+    private sha1 sha1;
+    private String username;
+    private String password;
+    private ServiceManager manager;
+    private MenuBean menu;
+    private Professor current_user;
+    private Discipline selectedDiscipline;
+
+    public Discipline getSelectedDiscipline() {
+        return selectedDiscipline;
+    }
+
+    public void setSelectedDiscipline(Discipline selectedDiscipline) {
+        this.selectedDiscipline = selectedDiscipline;
+    }
+
+    
+    
+    public MenuBean getMenu() {
+        return menu;
+    }
+
+    public void setMenu(MenuBean menu) {
+        this.menu = menu;
+    }
+
+    public void setSha1(sha1 sha1) {
+        this.sha1 = sha1;
+    }
+
+    public sha1 getSha1() {
+        return sha1;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String checkValidUser() {
+        System.out.println("DEBUG: USER: " + username + " TRYING TO LOGIN");
+        try {
+            manager = new ServiceManager();
+
+            if (manager.setAuthentication(username, sha1.parseSHA1Password(password))) {
+
+                current_user = manager.getCurrentProfessor();
+                manager.userService().updateDisciplines(current_user.getId());
+
+                this.menu = new MenuBean(current_user.getDisciplines());
+
+                System.out.println("DEBUG: USER: " + username + " SUCCESS");
+                return "success";
+            }
+
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(userManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(userManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(userManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(userManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(userManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Wrong User or Password"));
+        System.out.println("DEBUG: USER: " + username + " FAIL");
+        return "fail";
+
+    }
+
+    public String logOff() {
+        //TODO: Propper logout
+        try {
+            System.out.println("DEBUG: Redirecting to Logoff");
+            manager.closeConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(userManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "logoff";
+
+    }
+
+    public void redirectAddTest(ActionEvent event) {
+        Object obj = event.getSource();
+        MenuItem aux_info = (MenuItem) obj;
+        Submenu aux_discipline = (Submenu) aux_info.getParent();
+        selectedDiscipline = manager.userService().getDiscipline(aux_discipline.getLabel());
+        System.out.println("DEBUG: SELECTED DISCIPLINE: " + selectedDiscipline.name + " ID: " + selectedDiscipline.getId());
+
+    }
+    
+    public String redirectAddTest() {
+       System.out.println("DEBUG: Redirecting to addTest");
+        return "addTest";
+    }
+    
+    public void redirectEditContents(ActionEvent event) {
+        Object obj = event.getSource();
+        MenuItem aux_info = (MenuItem) obj;
+        Submenu aux_discipline = (Submenu) aux_info.getParent();
+        selectedDiscipline = manager.userService().getDiscipline(aux_discipline.getLabel());
+        System.out.println("DEBUG: SELECTED DISCIPLINE: " + selectedDiscipline.name + " ID: " + selectedDiscipline.getId());
+
+    }
+    
+    public String redirectEditContents() {
+       System.out.println("DEBUG: Redirecting to Edit Contents");
+        return "editContents";
+    }
+}
